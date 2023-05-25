1870dc3655dbd17df669819107cf6b1f368e73ca
Sandu-tt <tharakasanu80@gmail.com>
2023-05-26 02:02:49 +0530

    done spa assignment

---
+++ assignment/js/spa/javaScript.js
@@ -0,0 +1,13 @@
+document.getElementById("USER")
+document.getElementById("item").style.display="none";
+document.getElementById("orders").style.display="none";
+document.getElementById("placeOrders").style.display="none";
+
+document.getElementById("USER").addEventListener("click",function (){
+    document.getElementById("USERr").style.display="block"
+    document.getElementById("item").style.display="none";
+    document.getElementById("orders").style.display="none";
+    document.getElementById("placeOrders").style.display="none";
+});
+
+
