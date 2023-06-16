79d296ee651fd66204cd707e28d26b0caceb1055
Sandu-tt <tharakasanu80@gmail.com>
2023-06-16 13:35:51 +0530

    done changes in project section

--- assignment/js/spa/controller/controller.js
+++ assignment/js/spa/controller/controller.js
@@ -1,27 +1,36 @@
-$("#order-btn").click(function () {
-    $(".order").css("display", "inline-block");
-    $(".dashboard").css("display", "none");
-    $(".item").css("display", "none");
-    $(".customer").css("display", "none");
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("homePage").style.display = 'inline-block';
+    document.getElementById("customerPage").style.display = 'none';
+    document.getElementById("itemPage").style.display = 'none';
+    document.getElementById("orderPage").style.display = 'none';
 });
 
-$("#item-btn").click(function () {
-    $(".order").css("display", "none");
-    $(".dashboard").css("display", "none");
-    $(".item").css("display", "inline-block");
-    $(".customer").css("display", "none");
+document.getElementById("home").addEventListener("click", function () {
+    document.getElementById("homePage").style.display = 'block';
+    document.getElementById("customerPage").style.display = 'none';
+    document.getElementById("itemPage").style.display = 'none';
+    document.getElementById("orderPage").style.display = 'none';
 });
 
-$("#customer-btn").click(function () {
-    $(".order").css("display", "none");
-    $(".dashboard").css("display", "none");
-    $(".item").css("display", "none");
-    $(".customer").css("display", "inline-block");
+document.getElementById("items").addEventListener("click", function () {
+    document.getElementById("homePage").style.display = 'none';
+    document.getElementById("customerPage").style.display = 'none';
+    document.getElementById("itemPage").style.display = 'block';
+    document.getElementById("orderPage").style.display = 'none';
 });
 
-$("#dashboard-btn").click(function () {
-    $(".order").css("display", "none");
-    $(".dashboard").css("display", "block");
-    $(".item").css("display", "none");
-    $(".customer").css("display", "none");
+document.getElementById("customer").addEventListener("click", function () {
+    document.getElementById("homePage").style.display = "none";
+    document.getElementById("customerPage").style.display = 'block';
+    document.getElementById("itemPage").style.display = 'none';
+    document.getElementById("orderPage").style.display = 'none';
 });
+
+document.getElementById("orders").addEventListener("click", function () {
+    document.getElementById("homePage").style.display = 'none';
+    document.getElementById("customerPage").style.display = 'none';
+    document.getElementById("itemPage").style.display = 'none';
+    document.getElementById("orderPage").style.display = 'block';
+});
+
+
